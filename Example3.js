89a27e78560fcce7681e88a5f784acbf12a8a783
CBasni <149559172+CBasni@users.noreply.github.com>
2025-01-28 23:21:34 -0600

    Added Completed Drawing with P5 Assignment
    Recreated to look similar to the examples provided on Moodle

---
+++ Example3.js
@@ -0,0 +1,49 @@
+function setup() {
+
+  createCanvas(200, 100);
+  colorMode(HSB);
+
+  background(200);
+
+}
+
+function draw() {
+
+  background(0, 0, 0);
+
+  noStroke();
+
+  // Update start and stop angles.
+  let biteSize = PI / 16;
+  let startAngle = biteSize * sin(frameCount * 0.1) + biteSize;
+  let endAngle = TWO_PI - startAngle;
+
+  // Draw the arc.
+  fill(60, 100, 100);
+  arc(50, 50, 80, 80, startAngle, endAngle, PIE);
+
+  fill(12, 88, 82);
+  square(110, 10, 80);
+
+  fill(0, 0, 0);
+  rect(110, -40, 80);
+
+  fill(12, 88, 82);
+  circle(150, 50, 81.3);
+
+  fill(0, 0, 100);
+  circle(130, 50, 23);
+
+  fill(0, 0, 100);
+  circle(170, 50, 23);
+
+  fill(240, 100, 100);
+  circle(130, 50, 14);
+
+  fill(240, 100, 100);
+  circle(170, 50, 14);
+  
+}
+
+
+
