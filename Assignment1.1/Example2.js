5ab9f72805af2aee1847a6e07db777b4cd21b929
CBasni <149559172+CBasni@users.noreply.github.com>
2025-02-18 21:56:23 -0600

    Added Drawing With P5 Assignment

---
+++ Assignment1.1/Example2.js
@@ -0,0 +1,30 @@
+// Copy & Paste code to 'sketch.js' and then click on 'Go Live' on the bottom-right corner of VS Code
+
+function setup() {
+
+  createCanvas(200, 200);
+  colorMode(HSB);
+
+  background(200);
+
+}
+
+function draw() {
+
+  background(0, 0, 100);
+
+  noStroke(); //Removes outlines
+
+  fill(349, 45, 100, 0.8); 
+  ellipse(100, 66, 95, 97);
+
+  fill(120, 100, 100, 0.4);
+  ellipse(130, 126, 95, 97);
+
+  fill(232, 56, 90, 0.6); 
+  ellipse(70, 126, 95, 97);
+
+}
+
+
+
