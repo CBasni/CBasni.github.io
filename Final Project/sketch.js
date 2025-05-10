48d82daebfb237c20b7251c326d9d943ec7a405c
CBasni <149559172+CBasni@users.noreply.github.com>
2025-05-10 02:30:01 -0500

    Create sketch.js

---
+++ Final Project/sketch.js
@@ -0,0 +1,7 @@
+function setup() {
+  createCanvas(400, 400);
+  background(200);
+  textSize(24);
+  textAlign(CENTER, CENTER);
+  text('My Final Project Documentation!', width / 2, height / 2);
+}
